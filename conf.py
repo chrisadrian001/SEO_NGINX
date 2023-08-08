e5c208c6474bd3fd2e25d0113e146e5deee9ba7c
chrisadrian001 <chrisadrian001@users.noreply.github.com>
2023-08-08 10:14:37 -0700

    Updates

--- conf.py
+++ conf.py
@@ -11,49 +11,77 @@ copyright = '2023, c.adrian@f5.com'
 author = 'c.adrian@f5.com'
 release = '1'
 
-# -- Project information -----------------------------------------------------
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+# html_theme = 'alabaster'
+html_theme_path = ['docs/']
+html_theme = 'f5_sphinx_theme'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+# html_theme_options = {}
+html_theme_options = {
+    'next_prev_link': True
+}
+
+
 
 
 # -- General configuration ---------------------------------------------------
-# -- General configuration
 
-extensions = [
-    "sphinx.ext.duration",
-    "sphinx.ext.doctest",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.intersphinx",
-]
+#----- extensions = [
+#-----     "sphinx.ext.duration",
+#-----     "sphinx.ext.doctest",
+#-----     "sphinx.ext.autodoc",
+#-----     "sphinx.ext.autosummary",
+#-----     "sphinx.ext.intersphinx",
+#----- ]
 
-intersphinx_mapping = {
-    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
-    "python": ("https://docs.python.org/3/", None),
-    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
-}
-intersphinx_disabled_domains = ["std"]
+#----- intersphinx_mapping = {
+#-----     "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+#-----     "python": ("https://docs.python.org/3/", None),
+#-----     "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+#----- }
+#----- intersphinx_disabled_domains = ["std"]
 
-templates_path = ["_templates"]
+#----- templates_path = ["_templates"]
 
 # -- Options for EPUB output
-epub_show_urls = "footnote"
+#----- epub_show_urls = "footnote"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+#----- exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+#----- html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['docs/']
+#----- html_static_path = ['docs/']
